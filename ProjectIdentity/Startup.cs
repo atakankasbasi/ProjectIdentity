6c9cbdd8f1929ab5e0b3ca55ca9eb954331dea94
Muhammet-Dobaoglu <muhammetdobaoglu@hotmail.com>
2023-04-02 17:32:43 +0300

    ErrorPage

--- ProjectIdentity/Startup.cs
+++ ProjectIdentity/Startup.cs
@@ -47,6 +47,9 @@ namespace ProjectIdentity
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+            app.UseStatusCodePagesWithReExecute("/Home/ErrorPage", "?code={0}");
+
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
