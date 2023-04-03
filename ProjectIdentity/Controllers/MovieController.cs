b0d98ee12d83c30ffb77149b323c8f2aa36b89a2
Muhammet-Dobaoglu <muhammetdobaoglu@hotmail.com>
2023-04-03 15:44:40 +0300

    Devam

---
+++ ProjectIdentity/Controllers/MovieController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProjectIdentity.Controllers
+{
+    public class MovieController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
