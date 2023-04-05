396f8dae8c69b3b50d42b2366849fd0291dfb2b7
Muhammet <muhammetdobaoglu@hotmail.com>
2023-04-05 19:30:24 +0300

    Identity Giriş

--- ProjectIdentity/Controllers/DefaultController.cs
+++ ProjectIdentity/Controllers/DefaultController.cs
@@ -4,15 +4,29 @@ using ProjectIdentity.Models;
 using System.Net.Http;
 using System.Threading.Tasks;
 using System;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
 
 namespace ProjectIdentity.Controllers
 {
     public class DefaultController : Controller
     {
-        public IActionResult Index()
+        private readonly UserManager<AppUser> _userManager;
+
+        public DefaultController(UserManager<AppUser> userManager)
         {
-            return View();
+            _userManager = userManager;
         }
 
+        public async Task<IActionResult> Index()
+        {
+            var values = await _userManager.FindByNameAsync(User.Identity.Name);
+            UserEditViewModel userEditViewModel = new UserEditViewModel();
+            userEditViewModel.Name = values.Name;
+            userEditViewModel.Surname = values.Surname;
+            userEditViewModel.Mail = values.Email;
+            userEditViewModel.Phone = values.PhoneNumber;
+            return View(userEditViewModel);
+        }
     }
 }
